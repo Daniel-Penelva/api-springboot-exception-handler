59551ca65971a416b38eadc3a3b47f9564c91b44
Daniel Penelva <d4n.andrade@gmail.com>
2023-12-08 19:21:33 -0300

    Criando projeto api Spring boot 3 para utilizar exception handling

---
+++ src/main/java/com/api/apispringbootexceptionhandler/ApiSpringbootExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.api.apispringbootexceptionhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootExceptionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootExceptionHandlerApplication.class, args);
+	}
+
+}
